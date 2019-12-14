95478fd54d3d8150a5c01f965ce907b786bddc31
ErnstSmith <ernstsmith88@gmail.com>
2019-12-14 18:25:22 +0500

    Updates

---
+++ precache-manifest.4ad183496358bae06be8625037f24678.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a849e6a3c016a2284ad08d69524d2261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50283ac3f2f8f16d92ae",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d5430040a4d4956eccf5",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "50283ac3f2f8f16d92ae",
+    "url": "/static/js/2.15c7126c.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/static/js/2.15c7126c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5430040a4d4956eccf5",
+    "url": "/static/js/main.f2fd30f0.chunk.js"
+  },
+  {
+    "revision": "cd9d01b64bece67dfbbd",
+    "url": "/static/js/runtime-main.adcf3d04.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
