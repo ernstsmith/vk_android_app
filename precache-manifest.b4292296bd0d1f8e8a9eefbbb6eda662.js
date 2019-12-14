b62cb898344ea1c035dafe9a489534310373f061
ErnstSmith <ernstsmith88@gmail.com>
2019-12-14 19:41:23 +0500

    Updates

---
+++ precache-manifest.b4292296bd0d1f8e8a9eefbbb6eda662.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6532d1cc1bb7907a6c42a6446337c13c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "479b50bd6e6a9bcdd199",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d5430040a4d4956eccf5",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "479b50bd6e6a9bcdd199",
+    "url": "./static/js/2.496766f0.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "./static/js/2.496766f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5430040a4d4956eccf5",
+    "url": "./static/js/main.f2fd30f0.chunk.js"
+  },
+  {
+    "revision": "a8e61658d0fd66cc8bd4",
+    "url": "./static/js/runtime-main.271863f0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ad183496358bae06be8625037f24678.js"
+  "./precache-manifest.b4292296bd0d1f8e8a9eefbbb6eda662.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
